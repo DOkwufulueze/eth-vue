7724e1efd52dbbc1d35084aef6372482cc0bd084
Daniel Okwufulueze <danielokwufulueze@gmail.com>
2020-11-08 12:27:07 +0100

    Configuration:
    - configure DApp page.
    - configure pwa.

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 module.exports = {
+  configureWebpack: {
+    devtool: "cheap-source-map",
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          default: false
+        }
+      }
+    }
+  },
   devServer: {
     // hot reload
     hotOnly: true,
@@ -9,7 +19,28 @@ module.exports = {
     // server port
     port: 3001
   },
-  configureWebpack: {
-    devtool: "cheap-source-map"
+  lintOnSave: false,
+  outputDir: "dist",
+  pages: {
+    index: {
+      // entry point to eth-vue application
+      entry: "src/main.js",
+
+      // html file to use as template when serving or building
+      template: "index.html",
+
+      // file to distribute from build for production (in the dist directory)
+      filename: "index.html"
+    }
+  },
+  pwa: {
+    name: "eth-vue-icons",
+    iconPaths: {
+      appleTouchIcon: "./static/images/danielokwufulueze.png",
+      favicon16: "./static/images/danielokwufulueze.png",
+      favicon32: "./static/images/danielokwufulueze.png",
+      maskIcon: "./static/images/danielokwufulueze.png",
+      msTileImage: "./static/images/danielokwufulueze.png"
+    }
   }
 };
